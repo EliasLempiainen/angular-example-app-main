824fda30404cab6e9fd3c17a4e0ed3e78bb97013
Elias Lempiäinen <97599719+EliasLempiainen@users.noreply.github.com>
2023-12-13 12:48:52 +0200

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -11,6 +11,9 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent
+      ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
       ],
     }).compileComponents();
   });
@@ -27,10 +30,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angular-example-app');
   });
 
- it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-example-app app is running!');
-  });
+
 });
